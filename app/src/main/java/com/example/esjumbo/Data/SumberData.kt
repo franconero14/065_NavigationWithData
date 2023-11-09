1b3e937c2415b618b8dd4170578cac410e96e033
franconero14 <nerobalor@gmail.com>
2023-11-09 10:05:03 +0700

    first commit

---
+++ app/src/main/java/com/example/esjumbo/Data/SumberData.kt
@@ -0,0 +1,14 @@
+package com.example.esjumbo.Data
+
+
+import com.example.esjumbo.R
+
+object SumberData {
+    val flavours = listOf(
+        R.string.natural,
+        R.string.vanilla,
+        R.string.lemon,
+        R.string.milk,
+        R.string.lyche
+    )
+}
